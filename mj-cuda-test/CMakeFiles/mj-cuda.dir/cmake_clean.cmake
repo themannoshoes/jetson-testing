2a3fe491a532960c36d9307c9d366c3f133f1225
Wangmj <787633835@qq.com>
2021-03-08 18:55:53 +0800

    first commit

---
+++ mj-cuda-test/CMakeFiles/mj-cuda.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mj-cuda.dir/mj-cuda_generated_mj-cuda.cu.o"
+  "mj-cuda.pdb"
+  "mj-cuda"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/mj-cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
